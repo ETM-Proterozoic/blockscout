37b3be2f09601c584c6f0f95281bc9d6cf4c1aa8
LuxesOwen <66360114+LuxesOwen@users.noreply.github.com>
2022-12-21 17:34:30 +0800

    Update internal_transaction.ex

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -172,10 +172,10 @@ defmodule Indexer.Fetcher.InternalTransaction do
             try do
               EthereumJSONRPC.fetch_internal_transactions(transactions, json_rpc_named_arguments)
             catch
-              Logger.error(fn -> ["1failed to fetch internal transactions for blocks: ",Enum.join(transactions, ", "),Enum.join(json_rpc_named_arguments, ", ")] end,
-              error_count: 1
-              )
               :exit, error ->
+                Logger.error(fn -> ["1failed to fetch internal transactions for blocks: ",Enum.join(transactions, ", "),Enum.join(json_rpc_named_arguments, ", ")] end,
+                error_count: 1
+                )
                 {:error, error}
             end
         end
